871ff0b1c95b30a1fb99cf982f457b66619ac174
vitmvit <120332119+vitmvit@users.noreply.github.com>
2024-09-04 11:45:39 +0300

    refactor hibernate config

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -17,7 +17,14 @@ public class Util {
 
     public SessionFactory getSessionFactory() {
         Configuration configuration = new Configuration();
-        configuration.addAnnotatedClass(User.class);
+        configuration.addAnnotatedClass(User.class)
+                .setProperty("hibernate.driver.class", "org.postgresql.Driver")
+                .setProperty("hibernate.connection.url", "jdbc:postgresql://localhost:5432/user_hibernate_crud")
+                .setProperty("hibernate.connection.username", "root")
+                .setProperty("hibernate.connection.password", "root")
+                .setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect")
+                .setProperty("hibernate.show_sql", "true")
+                .setProperty("hibernate.current_session.context_class", "thread");
         return configuration.buildSessionFactory();
     }
 }
